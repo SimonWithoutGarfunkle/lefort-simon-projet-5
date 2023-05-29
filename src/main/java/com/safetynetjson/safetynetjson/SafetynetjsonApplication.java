aa11cb1bb764b8a10fb77a771a92364a1f924e83
SimonWithoutGarfunkle <123770440+SimonWithoutGarfunkle@users.noreply.github.com>
2023-05-29 15:30:18 +0200

    new start without H2 DB

---
+++ src/main/java/com/safetynetjson/safetynetjson/SafetynetjsonApplication.java
@@ -0,0 +1,13 @@
+package com.safetynetjson.safetynetjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetynetjsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetynetjsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safetynetjson/safetynetjson/SafetynetjsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetynetjson.safetynetjson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetynetjsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
