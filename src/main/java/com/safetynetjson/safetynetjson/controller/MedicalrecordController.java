dcce5c40d7a588873c173b25b6935da4008c0411
SimonWithoutGarfunkle <123770440+SimonWithoutGarfunkle@users.noreply.github.com>
2023-06-01 09:15:27 +0200

    add endpoint /medicalrecord

--- src/main/java/com/safetynetjson/safetynetjson/controller/MedicalrecordController.java
+++ src/main/java/com/safetynetjson/safetynetjson/controller/MedicalrecordController.java
@@ -1,5 +1,84 @@
 package com.safetynetjson.safetynetjson.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.safetynetjson.safetynetjson.model.Medicalrecord;
+import com.safetynetjson.safetynetjson.model.Medicalrecord;
+import com.safetynetjson.safetynetjson.model.JsonData;
+import com.safetynetjson.safetynetjson.service.JsonDataService;
+import com.safetynetjson.safetynetjson.service.MedicalrecordService;
+
+@RestController
 public class MedicalrecordController {
+	
+	
+	private final JsonDataService jsonDataService;
+	
+	private final MedicalrecordService medicalrecordService;
+	
+	public MedicalrecordController(JsonDataService jsonDataService, MedicalrecordService medicalrecordService) {
+        this.jsonDataService = jsonDataService;
+        this.medicalrecordService = medicalrecordService;
+    }
+	
+	@GetMapping("/medicalrecords")
+	public ResponseEntity<List<Medicalrecord>> getAllMedicalrecords() {
+		
+		JsonData jsonData = jsonDataService.getJsonData();
+
+		List<Medicalrecord> medicalrecords = jsonData.getMedicalrecords();
+	    
+	    return ResponseEntity.ok(medicalrecords);
+	}
+	
+	@PostMapping("/medicalrecord")
+    public ResponseEntity<String> postMedicalrecord(@RequestBody Medicalrecord medicalrecord) {
+		if (medicalrecordService.isPresent(medicalrecord)) {
+			return ResponseEntity.badRequest().body("Medicalrecord already exists");
+		}
+		medicalrecordService.addMedicalrecord(medicalrecord);
+        
+        return ResponseEntity.status(HttpStatus.CREATED).body("Medicalrecord added successfully");
+    }
+	
+	@PutMapping("/medicalrecord")
+    public ResponseEntity<String> putMedicalrecord(@RequestBody Medicalrecord medicalrecord) {
+		if (!(medicalrecordService.isPresent(medicalrecord))) {
+			return ResponseEntity.badRequest().body("Medicalrecord not found");
+		}
+		medicalrecordService.updateMedicalrecord(medicalrecord);
+        
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("FirstName and LastName cant be modified. Medicalrecord updated successfully");
+    }
+	
+	@PatchMapping("/medicalrecord")
+    public ResponseEntity<String> patchMedicalrecord(@RequestBody Medicalrecord medicalrecord) {
+		if (!(medicalrecordService.isPresent(medicalrecord))) {
+			return ResponseEntity.badRequest().body("Medicalrecord not found");
+		}
+		medicalrecordService.patchMedicalrecord(medicalrecord);
+        
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("FirstName and LastName cant be modified. Medicalrecord updated successfully");
+    }
+	
+	@DeleteMapping("/medicalrecord")
+    public ResponseEntity<String> deleteMedicalrecord(@RequestBody Medicalrecord medicalrecord) {
+		if (!(medicalrecordService.isPresent(medicalrecord))) {
+			return ResponseEntity.badRequest().body("Medicalrecord not found");
+		}
+		medicalrecordService.removeMedicalrecord(medicalrecord);
+        
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Medicalrecord removed successfully");
+    }
 
 }
--- src/main/java/com/safetynetjson/safetynetjson/service/MedicalrecordService.java
+++ src/main/java/com/safetynetjson/safetynetjson/service/MedicalrecordService.java
@@ -1,5 +1,100 @@
 package com.safetynetjson.safetynetjson.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.safetynetjson.safetynetjson.model.JsonData;
+import com.safetynetjson.safetynetjson.model.Medicalrecord;
+import com.safetynetjson.safetynetjson.model.Medicalrecord;
+
+@Service
 public class MedicalrecordService {
+	
+	private final JsonDataService jsonDataService;
+
+	public MedicalrecordService(JsonDataService jsonDataService) {
+		this.jsonDataService = jsonDataService;
+	}
+	
+	public boolean isPresent(Medicalrecord medicalrecord) {
+		JsonData jsonData = jsonDataService.getJsonData();
+		List<Medicalrecord> medicalrecords = jsonData.getMedicalrecords();
+		for (Medicalrecord record : medicalrecords) {
+			if (record.getFirstName().equals(medicalrecord.getFirstName())) {
+				if (record.getLastName().equals(medicalrecord.getLastName())) {
+					return true;
+				}
+			}
+
+		}
+		return false;
+
+	}
+
+	public void addMedicalrecord(Medicalrecord medicalrecord) {
+		JsonData jsonData = jsonDataService.getJsonData();
+		List<Medicalrecord> medicalrecords = jsonData.getMedicalrecords();
+
+		medicalrecords.add(medicalrecord);
+
+	}
+
+	public void removeMedicalrecord(Medicalrecord medicalrecord) {
+		JsonData jsonData = jsonDataService.getJsonData();
+		List<Medicalrecord> medicalrecords = jsonData.getMedicalrecords();
+		for (Medicalrecord record : medicalrecords) {
+			if (record.getFirstName().equals((medicalrecord).getFirstName())) {
+				if (record.getLastName().equals(medicalrecord.getLastName())) {
+					medicalrecords.remove(record);
+					break;
+				}
+
+			}
+		}
+
+	}
+	
+	public void updateMedicalrecord(Medicalrecord medicalrecord) {
+		JsonData jsonData = jsonDataService.getJsonData();
+		List<Medicalrecord> medicalrecords = jsonData.getMedicalrecords();
+		for (Medicalrecord record : medicalrecords) {
+			if (record.getFirstName().equals((medicalrecord).getFirstName())) {
+				if (record.getLastName().equals(medicalrecord.getLastName())) {
+					record.setBirthdate(medicalrecord.getBirthdate());
+					record.setMedications(medicalrecord.getMedications());
+					record.setAllergies(medicalrecord.getAllergies());
+				}
+
+			}
+		}
+
+	}
+
+	public void patchMedicalrecord(Medicalrecord medicalrecord) {
+		JsonData jsonData = jsonDataService.getJsonData();
+		List<Medicalrecord> medicalrecords = jsonData.getMedicalrecords();
+		for (Medicalrecord record : medicalrecords) {
+			if (record.getFirstName().equals((medicalrecord).getFirstName())) {
+				if (record.getLastName().equals(medicalrecord.getLastName())) {
+					if (!(medicalrecord.getBirthdate() == null)) {
+						record.setBirthdate(medicalrecord.getBirthdate());
+					}
+					if (!(medicalrecord.getMedications() == null)) {
+						record.setMedications(medicalrecord.getMedications());
+					}
+					if (!(medicalrecord.getAllergies() == null)) {
+						record.setAllergies(medicalrecord.getAllergies());
+
+					}
+
+
+				}
+
+			}
+		}
+
+	}
+
 
 }
